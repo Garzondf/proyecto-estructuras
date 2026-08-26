6c6f5142936d47c06b62e5dacf2e8c089e5bcef5
silvasarahy <silva_sarahy@javeriana.edu.co>
2026-08-26 17:50:11 -0500

    Completar los constructores y getters de Jugador y Territorio

--- Jugador.hpp
+++ Jugador.hpp
@@ -1,2 +1,47 @@
 #include "Jugador.h"
 #include "Territorio.h"
+
+using namespace std; 
+
+Jugador::Jugador(const string& nombre, const string& color) {
+    this->nombre = nombre;
+    this->color = color;
+    ejercito = 0;
+}
+
+void Jugador::AgregarTerritorio(Territorio* territorio) {
+    territorios.push_back(territorio);
+}
+
+void Jugador::EliminarTerritorio(Territorio* territorio) {
+    territorios.remove(territorio);
+}
+
+void Jugador::AgregarEjercito(int cantidad) {
+    ejercito += cantidad;
+}
+
+void Jugador::EliminarEjercito(int cantidad) {
+    ejercito -= cantidad;
+}
+
+const string& Jugador::ObtenerNombre() const {
+    return nombre; 
+}
+
+const string& Jugador::ObtenerColor() const { 
+    return color; 
+}
+
+int Jugador::ObtenerEjercito() const { 
+    return ejercito; 
+}
+
+const list<Territorio*>& Jugador::ObtenerTerritorios() const { 
+    return territorios; 
+}
+
+const vector<string>& Jugador::ObtenerCartas() const { 
+    return cartas; 
+}
+
--- Territorio.hpp
+++ Territorio.hpp
@@ -3,6 +3,50 @@
 
 using namespace std;
 
+Territorio::Territorio(const string& nombre, const string& codigo, const string& continente, Jugador* dueño, int unidades) {
+    this->nombre = nombre;
+    this->codigo = codigo;
+    this->continente = continente;
+    this->dueño = dueño;
+    this->unidades = unidades;
+}
+
+void Territorio::AgregarUnidades(int cantidad) { 
+    unidades += cantidad; 
+}
+
+void Territorio::EliminarUnidades(int cantidad) { 
+    unidades -= cantidad; 
+}
+
+void Territorio::CambiarDueño(Jugador* nuevoDueño) { 
+    dueño = nuevoDueño; 
+}
+
+const string& Territorio::ObtenerNombre() const { 
+    return nombre; 
+}
+
+const string& Territorio::ObtenerContinente() const { 
+    return continente; 
+}
+
+const string& Territorio::ObtenerCodigo() const { 
+    return codigo; 
+}
+
+int Territorio::ObtenerUnidades() const { 
+    return unidades; 
+}
+
+Jugador* Territorio::ObtenerDueño() const { 
+    return dueño; 
+}
+
+const list<Territorio*>& Territorio::ObtenerVecinos() const { 
+    return territoriosAdyacentes; 
+}
+
 void Territorio::AgregarVecino(Territorio* vecino) {
     // Verificamos que el vecino no se agregue dos veces (opcional pero seguro)
     for (Territorio* t : territoriosAdyacentes) {
